6b3373dce0e0975b4142214d5227f6fb75ab5afd
Teekay <tk.kayas44@gmail.com>
2025-06-29 20:25:27 +0200

    new file

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -1,20 +1,38 @@
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
 
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
 
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
+import unittest
+from simple_calculator import SimpleCalculator
 
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
+class TestSimpleCalculator(unittest.TestCase):
 
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+    def setUp(self):
+        """Set up a calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(0, 5), 5)
+        self.assertEqual(self.calc.add(-3, 3), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+        self.assertEqual(self.calc.subtract(10, 15), -5)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 99), 0)
+        self.assertEqual(self.calc.multiply(-2, -4), 8)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        self.assertEqual(self.calc.divide(7, -7), -1)
+        self.assertIsNone(self.calc.divide(5, 0))  # Edge case: divide by zero
+        self.assertAlmostEqual(self.calc.divide(10, 3), 3.3333333333, places=6)  # Floating point
+
+if __name__ == "__main__":
+    unittest.main()
